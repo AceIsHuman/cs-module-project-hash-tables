97c709031d1d476d77374bf3b4019f745b9faa12
AceIsHuman <menoman1995@yahoo.com>
2020-07-21 12:51:47 -0500

    populate words_to_follow dictionary with input.txt

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,12 +1,19 @@
 import random
-
+words = ''
 # Read in all the words in one go
 with open("input.txt") as f:
-    words = f.read()
+    words = f.read().split()
 
 # TODO: analyze which words can follow other words
-# Your code here
+words_to_follow = {}
 
+for i, word in enumerate(words):
+    if i == (len(words) - 1):
+        continue
+    elif words_to_follow.get(word) != None:
+        words_to_follow[word].append(words[i+1])
+    else:
+        words_to_follow[word] = [words[i+1]]
 
 # TODO: construct 5 random sentences
 # Your code here
